fb77103b739c81acea6e6125278732a70ab2a72a
robbbowes <up666724@myport.ac.uk>
2019-05-03 11:22:20 +0200

    Add sound on finish, centre textview, add blinking animation on pause

--- app/src/main/java/com/example/countdowntimerapp/MainActivity.java
+++ app/src/main/java/com/example/countdowntimerapp/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.countdowntimerapp;
 
+import android.media.MediaPlayer;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -17,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     boolean manuallyStopped;
     boolean isPaused;
     CountDownTimer countDownTimer;
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +28,8 @@ public class MainActivity extends AppCompatActivity {
 
         final TextView seekBarTextView = findViewById(R.id.seekBarTextView);
         SeekBar seekBarTimer = findViewById(R.id.seekBarTime);
-        final int max = 1200;
-        final int startingProgress = 600;
+        final int max = 600;
+        final int startingProgress = max / 2;
         timeToDisplayInt = startingProgress;
         seekBarTimer.setMax(max);
         seekBarTimer.setProgress(startingProgress);
@@ -36,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         seekBarTimer.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                int min = 1;
+                int min = 0;
                 if (progress < min) {
                     timeToDisplayInt = min;
                     seekBar.setProgress(min);
@@ -44,7 +47,6 @@ public class MainActivity extends AppCompatActivity {
                     timeToDisplayInt = progress;
                 }
                 seekBarTextView.setText(displayTimeCorrectly(timeToDisplayInt));
-
             }
 
             @Override
@@ -70,19 +72,30 @@ public class MainActivity extends AppCompatActivity {
     public void clickStop(View view) {
         startStopHelper(true);
         manuallyStopped = true;
+        SeekBar seekBar = findViewById(R.id.seekBarTime);
+        timeToDisplayInt = seekBar.getProgress();
+        TextView seekBarTextView = findViewById(R.id.seekBarTextView);
+        seekBarTextView.setText(displayTimeCorrectly(timeToDisplayInt));
         if (countDownTimer != null) {
+            setSolid(seekBarTextView);
             countDownTimer.cancel();
             countDownTimer.onFinish();
+        }
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
         }
     }
 
     public void clickPause(View view) {
         if (countDownTimer != null) {
+            TextView seekBarTextView = findViewById(R.id.seekBarTextView);
             if (!isPaused) {
                 countDownTimer.cancel();
+                makeBlink(seekBarTextView);
                 isPaused = true;
             } else {
                 countDownTimer.start();
+                setSolid(seekBarTextView);
                 isPaused = false;
             }
         }
@@ -101,11 +114,34 @@ public class MainActivity extends AppCompatActivity {
         return decimalFormat.format(minutes) + ":" + decimalFormat.format(seconds);
     }
 
+    private void setSolid(TextView textview) {
+        Animation animation = new AlphaAnimation(0.0f, 1.0f);
+        textview.startAnimation(animation);
+    }
+
+    private void makeBlink(TextView textView) {
+        Animation animation = new AlphaAnimation(0.0f, 1.0f);
+        animation.setDuration(500);
+        animation.setStartOffset(20);
+        animation.setRepeatMode(Animation.REVERSE);
+        animation.setRepeatCount(Animation.INFINITE);
+        textView.startAnimation(animation);
+    }
+
+    private void playBeeper() {
+        mediaPlayer = MediaPlayer.create(this, R.raw.beeper);
+        mediaPlayer.start();
+    }
+
     private CountDownTimer createTimer() {
         return new CountDownTimer(timeToDisplayInt * 1000, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
                 timeToDisplayInt -= 1;
+                TextView seekBarTextView = findViewById(R.id.seekBarTextView);
+                seekBarTextView.setText(displayTimeCorrectly(timeToDisplayInt));
+                SeekBar seekBar = findViewById(R.id.seekBarTime);
+                seekBar.setProgress(timeToDisplayInt);
                 Log.i("Counting", String.valueOf(timeToDisplayInt));
             }
 
@@ -113,6 +149,7 @@ public class MainActivity extends AppCompatActivity {
             public void onFinish() {
                 if (!manuallyStopped) {
                     Log.i("onFinish()", "Ran to end");
+                    playBeeper();
                 } else {
                     Log.i("onFinish()", "Manually stopped");
                 }
