0bd1f77aface63713726d939db1f38ec216234e6
robbbowes <up666724@myport.ac.uk>
2019-05-03 09:14:45 +0200

    Add buttons and enabled/disable functionality

--- app/src/main/java/com/example/countdowntimerapp/MainActivity.java
+++ app/src/main/java/com/example/countdowntimerapp/MainActivity.java
@@ -2,13 +2,14 @@ package com.example.countdowntimerapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +18,12 @@ public class MainActivity extends AppCompatActivity {
 
         final TextView seekBarTextView = findViewById(R.id.seekBarTextView);
         SeekBar seekBarTimer = findViewById(R.id.seekBarTime);
-        seekBarTimer.setMax(1200);
+        final int max = 1200;
+        final int startingProgress = 600;
+        seekBarTimer.setMax(max);
+        seekBarTimer.setProgress(startingProgress);
+        String startingProgressString = displayTimeCorrectly(startingProgress);
+        seekBarTextView.setText(startingProgressString);
 
         seekBarTimer.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -46,10 +52,24 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    public void clickStart(View view) {
+        startStopHelper(false);
+    }
+
+    public void clickStop(View view) {
+        startStopHelper(true);
+    }
+
+    private void startStopHelper(boolean enable) {
+        SeekBar seekBar = findViewById(R.id.seekBarTime);
+        seekBar.setEnabled(enable);
+        findViewById(R.id.startButton).setEnabled(enable);
+    }
+
     public String displayTimeCorrectly(int i) {
         int minutes = i / 60;
         int seconds = i % 60;
         DecimalFormat decimalFormat = new DecimalFormat("00");
-        return minutes + ":" + decimalFormat.format(seconds);
+        return decimalFormat.format(minutes) + ":" + decimalFormat.format(seconds);
     }
 }
